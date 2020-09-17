31cab2579de4736ed7eb3489be68da9cf9bcd598
Pungitius <robert.kosmehl@gmx.de>
2020-09-18 00:00:53 +0200

    added png download added svg download

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const RESOLUTION = 1000;
 const DRAW_LINES = true;
-const DEFAULT_AIRFOIL = '2412';
+const DEFAULT_AIRFOIL = "2412";
 
-var canvas;
-var ctx;
-var plotButton;
-var downloadButton;
-var digitInput;
+let canvas;
+let ctx;
+let plotButton;
+let downloadButton;
+let digitInput;
 
 let currentAirfoil = DEFAULT_AIRFOIL;
+let points;
 
 window.onload = (event) => {
-    canvas = document.getElementById('plot');
-    ctx = canvas.getContext('2d');
+    canvas = document.getElementById("plot");
+    ctx = canvas.getContext("2d");
 
-    plotButton = document.getElementById('plotButton');
-    downloadButton = document.getElementById('downloadButton');
+    plotButton = document.getElementById("plotButton");
+    downloadPNG = document.getElementById("downloadPNG");
+    downloadSVG = document.getElementById("downloadSVG");
 
-    digitInput = document.getElementById('digits');
+    digitInput = document.getElementById("digits");
 
-    errorDiv = document.getElementById('input_error');
+    errorDiv = document.getElementById("input_error");
 
     digitInput.addEventListener("keydown", function (event) {
         console.log(digits);
@@ -27,23 +29,51 @@ window.onload = (event) => {
             event.preventDefault();
             plotButton.click();
         }
-    }
-    );
+    });
 
-    plotButton.addEventListener('click', () => {
+    plotButton.addEventListener("click", () => {
         currentAirfoil = digitInput.value;
         var error = airfoilFromString(digitInput.value);
         if (error.length > 0) {
-
             errorDiv.innerText = error;
-            errorDiv.style.display = 'block';
+            errorDiv.style.display = "block";
         } else {
-            errorDiv.style.display = 'none';
+            errorDiv.style.display = "none";
         }
-    }
-    );
+    });
 
-    window.addEventListener('resize', () => { airfoilFromString(currentAirfoil) }, false);
+    downloadPNG.addEventListener("click", () => {
+        let link = document.createElement("a");
+        link.download = 'NACA' + currentAirfoil + ".png";
+        link.href = document.getElementById("plot").toDataURL();
+        link.click();
+    });
+
+    downloadSVG.addEventListener("click", () => {
+        let m = .01 * parseFloat(currentAirfoil.charAt(0));
+        let t = .01 * parseFloat(currentAirfoil.charAt(2) + currentAirfoil.charAt(3));
+        let link = document.createElement("a");
+
+        let svgString = 'data:image/svg+xml, <svg xmlns="http://www.w3.org/2000/svg"> ';
+        svgString += '<polygon points="';
+        for (let i = 0; i < points.length; i++) {
+            svgString += 1000 * points[i][0] + ',' + 1000 * (m + .5 * t + points[i][1]) + ' ';
+        }
+        svgString += '" stroke="%234169E1" stroke-width="1" fill="%23d9e2ff"/>';
+        svgString += ' </svg>';
+
+        link.href = svgString;
+        link.download = 'NACA' + currentAirfoil + ".svg";
+        link.click();
+    });
+
+    window.addEventListener(
+        "resize",
+        () => {
+            airfoilFromString(currentAirfoil);
+        },
+        false
+    );
 
     airfoilFromString(DEFAULT_AIRFOIL);
 };
@@ -54,24 +84,29 @@ function resizeCanvas(x, y) {
 }
 
 function airfoilFromString(text) {
-    let chars = text.split('');
+    let chars = text.split("");
 
     var error = [];
 
     if (text.length < 4) {
-        error.push('input is too short. ');
+        error.push("input is too short. ");
     }
     if (text.length > 4) {
-        error.push('input is too long. ');
+        error.push("input is too long. ");
     }
     for (let i = 0; i < chars.length; i++) {
         if (isNaN(chars[i])) {
-            error.push('input is not a number. ');
+            error.push("input is not a number. ");
             break;
         }
     }
     if (error.length === 0) {
-        NACA4Digit(1, .01 * chars[0], .1 * chars[1], .01 * (chars[2] + chars[3]));
+        NACA4Digit(
+            1,
+            0.01 * chars[0],
+            0.1 * chars[1],
+            0.01 * (chars[2] + chars[3])
+        );
     }
 
     return error;
@@ -82,34 +117,52 @@ function debug() {
 }
 
 function pad(n, width, z) {
-    z = z || '0';
-    n = n + '';
+    z = z || "0";
+    n = n + "";
     return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-  }
+}
 
-function NACA4Digit(c, m, p, t) //c = camber length
-{
-    resizeCanvas(window.innerWidth, .1 * window.innerWidth + window.innerWidth * (t + 2 * m));
+function NACA4Digit(c, m, p, t) {
+    //c = camber length
+    resizeCanvas(
+        window.innerWidth,
+        0.1 * window.innerWidth + window.innerWidth * (t + 2 * m)
+    );
 
     ctx.resetTransform();
 
-    console.log('called gen');
+    console.log("called gen");
     ctx.fillStyle = "rgba(0, 0, 0, 1)";
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.font = .02 * canvas.width + 'px serif';
-    ctx.fillStyle = '#4169E1';
-    ctx.fillText('NACA' + (100 * m).toFixed(0) + (10 * p).toFixed(0) + pad((100 * t).toFixed(0), 2), .8 * canvas.width, .9 * canvas.height);
+    ctx.font = 0.02 * canvas.width + "px serif";
+    ctx.fillStyle = "#4169E1";
+    ctx.fillText(
+        "NACA" +
+        (100 * m).toFixed(0) +
+        (10 * p).toFixed(0) +
+        pad((100 * t).toFixed(0), 2),
+        0.8 * canvas.width,
+        0.9 * canvas.height
+    );
 
-    ctx.translate(0, canvas.height * .5);
+    ctx.translate(0, canvas.height * 0.5);
 
     let step = 1 / RESOLUTION;
 
     // upper half
+    points = [];
     ctx.beginPath();
     for (let x = 0; x < 1; x += step) {
-        let thickness = 5 * t * (0.2969 * Math.sqrt(x) - 0.1260 * x - 0.3516 * Math.pow(x, 2) + 0.2843 * Math.pow(x, 3) - 0.1036 * Math.pow(x, 4));//adjusted for zero thickness trailing edge
+        let thickness =
+            5 *
+            t *
+            (0.2969 * Math.sqrt(x) -
+                0.126 * x -
+                0.3516 * Math.pow(x, 2) +
+                0.2843 * Math.pow(x, 3) -
+                0.1036 * Math.pow(x, 4)); //adjusted for zero thickness trailing edge
 
         var camber;
         var theta;
@@ -117,14 +170,15 @@ function NACA4Digit(c, m, p, t) //c = camber length
         if (m === 0 && p === 0) {
             camber = 0;
             theta = Math.PI;
-        }
-        else {
+        } else {
             if (x <= p * c) {
                 camber = (m / Math.pow(p, 2)) * (2 * p * (x / c) - Math.pow(x / c, 2));
-                theta = Math.atan((2 * m / (Math.pow(p, 2))) * (p - (x / c)));
+                theta = Math.atan(((2 * m) / Math.pow(p, 2)) * (p - x / c));
             } else if (x <= c) {
-                camber = (m / Math.pow(1 - p, 2)) * ((1 - 2 * p) + 2 * p * (x / c) - Math.pow(x / c, 2));
-                theta = Math.atan((2 * m / (Math.pow(1 - p, 2)) * (p - x / c)));
+                camber =
+                    (m / Math.pow(1 - p, 2)) *
+                    (1 - 2 * p + 2 * p * (x / c) - Math.pow(x / c, 2));
+                theta = Math.atan(((2 * m) / Math.pow(1 - p, 2)) * (p - x / c));
             }
             camber *= -1;
         }
@@ -136,9 +190,17 @@ function NACA4Digit(c, m, p, t) //c = camber length
         //stroke(outlineColor);
         if (DRAW_LINES) {
             if (x > 0) {
-                ctx.lineTo(xU * .8 * canvas.width + .1 * canvas.width, yU * .8 * canvas.width);
+                points.push([xU, yU]);
+                ctx.lineTo(
+                    xU * 0.8 * canvas.width + 0.1 * canvas.width,
+                    yU * 0.8 * canvas.width
+                );
             } else {
-                ctx.moveTo(xU * .8 * canvas.width + .1 * canvas.width, yU * .8 * canvas.width);
+                points.push([xU, yU]);
+                ctx.moveTo(
+                    xU * 0.8 * canvas.width + 0.1 * canvas.width,
+                    yU * 0.8 * canvas.width
+                );
             }
         }
     }
@@ -146,7 +208,14 @@ function NACA4Digit(c, m, p, t) //c = camber length
     // lower half
     for (let x = 1; x >= 0; x -= step) {
         //let thickness = 5*t*(0.2969*sqrt(x)-0.1260*x-0.3516*pow(x, 2)+0.2843*pow(x, 3)-0.1015*pow(x, 4));
-        let thickness = 5 * t * (0.2969 * Math.sqrt(x) - 0.1260 * x - 0.3516 * Math.pow(x, 2) + 0.2843 * Math.pow(x, 3) - 0.1036 * Math.pow(x, 4));//adjusted for zero thickness trailing edge
+        let thickness =
+            5 *
+            t *
+            (0.2969 * Math.sqrt(x) -
+                0.126 * x -
+                0.3516 * Math.pow(x, 2) +
+                0.2843 * Math.pow(x, 3) -
+                0.1036 * Math.pow(x, 4)); //adjusted for zero thickness trailing edge
 
         var camber;
         var theta;
@@ -154,14 +223,15 @@ function NACA4Digit(c, m, p, t) //c = camber length
         if (m === 0 && p === 0) {
             camber = 0;
             theta = Math.PI;
-        }
-        else {
+        } else {
             if (x <= p * c) {
                 camber = (m / Math.pow(p, 2)) * (2 * p * (x / c) - Math.pow(x / c, 2));
-                theta = Math.atan((2 * m / (Math.pow(p, 2))) * (p - (x / c)));
+                theta = Math.atan(((2 * m) / Math.pow(p, 2)) * (p - x / c));
             } else if (x <= c) {
-                camber = (m / Math.pow(1 - p, 2)) * ((1 - 2 * p) + 2 * p * (x / c) - Math.pow(x / c, 2));
-                theta = Math.atan((2 * m / (Math.pow(1 - p, 2)) * (p - x / c)));
+                camber =
+                    (m / Math.pow(1 - p, 2)) *
+                    (1 - 2 * p + 2 * p * (x / c) - Math.pow(x / c, 2));
+                theta = Math.atan(((2 * m) / Math.pow(1 - p, 2)) * (p - x / c));
             }
             camber *= -1;
         }
@@ -173,11 +243,18 @@ function NACA4Digit(c, m, p, t) //c = camber length
         //stroke(outlineColor);
         if (DRAW_LINES) {
             if (x > 0) {
-                ctx.lineTo(xL * .8 * canvas.width + .1 * canvas.width, yL * .8 * canvas.width);
+                points.push([xL, yL]);
+                ctx.lineTo(
+                    xL * 0.8 * canvas.width + 0.1 * canvas.width,
+                    yL * 0.8 * canvas.width
+                );
             } else {
                 //ctx.beginPath();
-                ctx.lineTo(xL * .8 * canvas.width + .1 * canvas.width, yL * .8 * canvas.width);
-
+                points.push([xL, yL]);
+                ctx.lineTo(
+                    xL * 0.8 * canvas.width + 0.1 * canvas.width,
+                    yL * 0.8 * canvas.width
+                );
             }
         }
     }
@@ -186,12 +263,12 @@ function NACA4Digit(c, m, p, t) //c = camber length
 
     ctx.fillStyle = "#d9e2ff";
     ctx.fill();
-    ctx.strokeStyle = '#4169E1';
+    ctx.strokeStyle = "#4169E1";
     ctx.setLineDash([]);
-    ctx.stroke()
+    ctx.stroke();
 
     // camber
-    if (document.getElementById('camberCheck').checked) {
+    if (document.getElementById("camberCheck").checked) {
         ctx.beginPath();
         for (let x = 0; x < 1; x += step) {
             var camber;
@@ -199,37 +276,37 @@ function NACA4Digit(c, m, p, t) //c = camber length
             if (m === 0 && p === 0) {
                 camber = 0;
                 theta = Math.PI;
-            }
-            else {
+            } else {
                 if (x <= p * c) {
-                    camber = (m / Math.pow(p, 2)) * (2 * p * (x / c) - Math.pow(x / c, 2));
+                    camber =
+                        (m / Math.pow(p, 2)) * (2 * p * (x / c) - Math.pow(x / c, 2));
                 } else if (x <= c) {
-                    camber = (m / Math.pow(1 - p, 2)) * ((1 - 2 * p) + 2 * p * (x / c) - Math.pow(x / c, 2));
+                    camber =
+                        (m / Math.pow(1 - p, 2)) *
+                        (1 - 2 * p + 2 * p * (x / c) - Math.pow(x / c, 2));
                 }
                 camber *= -1;
             }
 
-            ctx.lineTo(x * .8 * canvas.width + .1 * canvas.width, camber * .8 * canvas.width);
+            ctx.lineTo(
+                x * 0.8 * canvas.width + 0.1 * canvas.width,
+                camber * 0.8 * canvas.width
+            );
 
             ctx.setLineDash([3, 5]);
-            ctx.strokeStyle = '#4169E1';
-            ctx.stroke()
+            ctx.strokeStyle = "#4169E1";
+            ctx.stroke();
         }
     }
 
-
-    if (document.getElementById('chordCheck').checked) {
+    if (document.getElementById("chordCheck").checked) {
         // chord
         ctx.beginPath();
-        ctx.moveTo(.1 * canvas.width, 0);
-        ctx.lineTo(.9 * canvas.width, 0);
+        ctx.moveTo(0.1 * canvas.width, 0);
+        ctx.lineTo(0.9 * canvas.width, 0);
 
         ctx.setLineDash([2, 2, 5, 2]);
-        ctx.strokeStyle = '#4169E1';
-        ctx.stroke()
+        ctx.strokeStyle = "#4169E1";
+        ctx.stroke();
     }
-
-
-
 }
-
--- readme.md
+++ readme.md
@@ -3,5 +3,9 @@ A simple website that generates NACA Airfoils based on 4 input digits.
 Airfoils can be downloaded in SVG or PNG format.
 
 todo
-[ ] add download for png
-[ ] add download for svg
+[x] add download for png
+[x] add download for svg
+[ ] plot camber/chord on click
+[ ] check keycode for alternatives
+[ ]
+
